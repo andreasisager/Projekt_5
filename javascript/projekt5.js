4e3b5a3c250f2ad2dc5cb835f72f4d37c5e9be7e
SebastianSalling <sebastian-benjamin-s@hotmail.com>
2025-05-13 13:19:54 +0200

    Jquery footer og navbar

---
+++ javascript/projekt5.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    $("#navbar").load("navbar.html");
+});
+document.addEventListener("DOMContentLoaded", function () {
+    $("#footer-inject").load("footer.html");
+});
