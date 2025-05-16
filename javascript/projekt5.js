84d5c4a633cb7e967c285127a1f55d5e10d84722
SebastianSalling <sebastian-benjamin-s@hotmail.com>
2025-05-16 13:14:23 +0200

    Navbar og js transition

--- javascript/projekt5.js
+++ javascript/projekt5.js
@@ -4,7 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
     $("#footer-inject").load("footer.html");
 });
+
 document.addEventListener("DOMContentLoaded", function () {
     $("#productcard").load("productcard.html");
 });
     
+
+const nav = document.getElementById('home-navbar');
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+        nav.classList.add('transition');
+    }
+    else {
+        nav.classList.remove('transition');
+    }
+});
+
+
