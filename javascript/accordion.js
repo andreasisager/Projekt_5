04abad07ef47826f562d21d3c161d3891b2456b6
Loevstad14 <Loevstad14@gmail.com>
2025-05-23 13:51:57 +0200

    accordion ændringer (nu virker det?)

--- javascript/accordion.js
+++ javascript/accordion.js
@@ -1,14 +1,14 @@
-const accordions = Array.from(document.querySelectorAll('.accordion'));
-  const panels = Array.from(document.querySelectorAll('.panel'));
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-  accordions.forEach((accordion, index) => {
-    accordion.addEventListener('click', () => {
-        accordion.classList.toggle('active');
-        
-        if (panels[index].style.display === 'block') {
-            panels[index].style.display = 'none';
-          } else {
-            panels[index].style.display = 'block';
-          }
-        });
-      });
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
